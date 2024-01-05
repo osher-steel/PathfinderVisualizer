2fca0d6ec59b2ee5527ad9d818b6aaf83e9f548d
osher-steel <111786194+osher-steel@users.noreply.github.com>
2024-01-05 17:16:38 -0500

    Update AStar.h

--- headers/AStar.h
+++ headers/AStar.h
@@ -4,14 +4,6 @@
 
 class AStar: public Pathfinder{
     public:
-        // struct Compare {
-        //     bool operator()(const std::pair<int, sf::Vector2i>& left, const std::pair<int, sf::Vector2i>& right) const {
-        //         return left.first > right.first;  // Use '>' for min heap (lowest value at the top)
-        //     }
-        // };
-
-        // std::priority_queue<std::pair<int, sf::Vector2i>, std::vector<std::pair<int, sf::Vector2i>>, Compare> neighbors;
-
         std::vector<Cell*> neighbors;
         
         AStar(bool _eightDirectional):Pathfinder(_eightDirectional,false,false){};
