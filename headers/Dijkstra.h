5ffd2f7e285551d8532317c37d90a9a35c028993
osher-steel <111786194+osher-steel@users.noreply.github.com>
2024-01-05 17:16:12 -0500

    Update Dijkstra.h

--- headers/Dijkstra.h
+++ headers/Dijkstra.h
@@ -5,14 +5,6 @@
 
 class Dijkstra:public Pathfinder{
     public:
-        // struct Compare {
-        // bool operator()(const std::pair<int, sf::Vector2i>& left, const std::pair<int, sf::Vector2i>& right) const {
-        //     return left.first > right.first;  // Use '>' for min heap (lowest value at the top)
-        // }
-        // };
-
-        // std::priority_queue<std::pair<int, sf::Vector2i>, std::vector<std::pair<int, sf::Vector2i>>, Compare> neighbors;
-
         std::vector<Cell*> neighbors;
 
         Dijkstra(bool _eightDirectional): Pathfinder(_eightDirectional,false,false){};
